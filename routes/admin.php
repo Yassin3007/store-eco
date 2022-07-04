25905efd4dfa5b8971b9f27fc861b28fcfd26ecb
yassin ahmed <yassin.ahmed3070@gmail.com>
2022-07-04 02:07:53 +0200

    =end first branch

---
+++ routes/admin.php
@@ -0,0 +1,46 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+//note that there is prefix admin for all the page
+
+Route::get('/', function () {
+    return view('welcome');
+});
+route::group(['namespace'=>'Admin' ,'middleware' =>'auth:admin'] ,function(){
+    route::get('/' , 'DashboardController@index')->name('admin.dashboard') ;
+
+
+});
+route::group(['namespace'=>'Admin','middleware'=>'guest:admin'],function(){
+    route::get('login' , 'LoginController@login') ;
+    route::post('login' , 'LoginController@postLogin')->name('admin.post.login') ;
+});
+
+
+
+
+route::get('ad' , function(){
+    return 'admin loginnnnnnnn' ;
+})->name('admin.login');
+Route::group(['prefix' => LaravelLocalization::setLocale()], function()
+{
+    /** ADD ALL LOCALIZED ROUTES INSIDE THIS GROUP **/
+    Route::get('/hello', function()
+    {
+        return ('hello');
+    });
+
+
+});
